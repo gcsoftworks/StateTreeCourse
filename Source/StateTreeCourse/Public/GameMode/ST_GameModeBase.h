92dd2a84c19f50e98ca7bafc52a88972f72aeb81
gcsoftworks <128013217+gcsoftworks@users.noreply.github.com>
2026-08-20 10:09:38 +0700

    02_Create_Character_And_GameMode_Class

---
+++ Source/StateTreeCourse/Public/GameMode/ST_GameModeBase.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ST_GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STATETREECOURSE_API AST_GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+};

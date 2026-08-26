ef0931c18d02a8637bec20788c730324e358551a
gcsoftworks <128013217+gcsoftworks@users.noreply.github.com>
2026-08-26 11:56:37 +0700

    08_Create_Enemy_Character

---
+++ Source/StateTreeCourse/Private/Character/Enemy/ST_EnemyCharacter.cpp
@@ -0,0 +1,47 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/Enemy/ST_EnemyCharacter.h"
+
+#include "GameFramework/CharacterMovementComponent.h"
+
+
+// Sets default values
+AST_EnemyCharacter::AST_EnemyCharacter()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	AutoPossessAI = EAutoPossessAI::PlacedInWorldOrSpawned;
+	
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+	
+	GetCharacterMovement()->bUseControllerDesiredRotation = false;
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f);
+	GetCharacterMovement()->MaxWalkSpeed = 300.0f;
+	GetCharacterMovement()->MaxAcceleration = 1800.0f;
+	GetCharacterMovement()->BrakingDecelerationWalking = 2000.0f;
+}
+
+// Called when the game starts or when spawned
+void AST_EnemyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AST_EnemyCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void AST_EnemyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
---
+++ Source/StateTreeCourse/Public/Character/Enemy/ST_EnemyCharacter.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/ST_CharacterBase.h"
+#include "ST_EnemyCharacter.generated.h"
+
+UCLASS()
+class STATETREECOURSE_API AST_EnemyCharacter : public AST_CharacterBase
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AST_EnemyCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};

ac775f49aa58731a46c9010dd945e7c37f91cfa6
gcsoftworks <128013217+gcsoftworks@users.noreply.github.com>
2026-09-11 17:39:11 +0700

    12_Create_Play_Montage_And_Wait_Task

---
+++ Source/StateTreeCourse/Private/StateTree/Task/ST_PlayMontageAndWaitStateTreeTask.cpp
@@ -0,0 +1,154 @@
+﻿#include "StateTree/Task/ST_PlayMontageAndWaitStateTreeTask.h"
+
+#include "StateTreeExecutionContext.h"
+#include "GameFramework/Character.h"
+
+const UStruct* FST_PlayMontageAndWaitStateTreeTask::GetInstanceDataType() const
+{
+	return FST_PlayMontageAndWaitTaskInstanceData::StaticStruct();
+}
+
+EStateTreeRunStatus FST_PlayMontageAndWaitStateTreeTask::EnterState(FStateTreeExecutionContext& Context,
+	const FStateTreeTransitionResult& Transition) const
+{
+	FInstanceDataType& InstanceData = Context.GetInstanceData(*this);
+	
+	InstanceData.bIsFinished = false;
+	InstanceData.bStopWhenExitState = false;
+	
+	if (InstanceData.ContextActor == nullptr || InstanceData.MontageToPlay == nullptr)
+	{
+		return EStateTreeRunStatus::Failed;
+	}
+	
+	UAnimInstance* AnimInstance = 
+		Cast<ACharacter>(InstanceData.ContextActor)->GetMesh() ? 
+			Cast<ACharacter>(InstanceData.ContextActor)->GetMesh()->GetAnimInstance() : nullptr;
+	
+	if (AnimInstance == nullptr)
+	{
+		return EStateTreeRunStatus::Failed;
+	}
+	
+	const float Duration = AnimInstance->Montage_Play(InstanceData.MontageToPlay, InstanceData.PlayRate);
+	if (Duration <= 0.0f)
+	{
+		return EStateTreeRunStatus::Failed;
+	}
+	
+	if (InstanceData.StartSection != NAME_None)
+	{
+		AnimInstance->Montage_JumpToSection(InstanceData.StartSection, InstanceData.MontageToPlay);	
+	}
+	
+	FOnMontageEnded OnMontageEnded;
+	OnMontageEnded.BindLambda([&InstanceData](UAnimMontage* Montage, bool bInterrupted)
+	{
+		InstanceData.bIsFinished = true;
+		InstanceData.bWasInterrupted = bInterrupted;
+	});
+	
+	AnimInstance->Montage_SetEndDelegate(OnMontageEnded, InstanceData.MontageToPlay);
+	
+	return EStateTreeRunStatus::Running;
+}
+
+EStateTreeRunStatus FST_PlayMontageAndWaitStateTreeTask::Tick(FStateTreeExecutionContext& Context,
+	const float DeltaTime) const
+{
+	FInstanceDataType& InstanceData = Context.GetInstanceData(*this);
+	
+	if (InstanceData.bIsFinished)
+	{
+		return InstanceData.bWasInterrupted ? EStateTreeRunStatus::Failed : EStateTreeRunStatus::Succeeded;
+	}
+	
+	return EStateTreeRunStatus::Running;
+}
+
+void FST_PlayMontageAndWaitStateTreeTask::ExitState(FStateTreeExecutionContext& Context,
+	const FStateTreeTransitionResult& Transition) const
+{
+	FInstanceDataType& InstanceData = Context.GetInstanceData(*this);
+	
+	if (InstanceData.ContextActor == nullptr)
+	{
+		return;
+	}
+	
+	UAnimInstance* AnimInstance = 
+		Cast<ACharacter>(InstanceData.ContextActor)->GetMesh() ? 
+			Cast<ACharacter>(InstanceData.ContextActor)->GetMesh()->GetAnimInstance() : nullptr;
+	
+	if (AnimInstance == nullptr)
+	{
+		return;
+	}
+	
+	FOnMontageEnded OnMontageEnded;
+	AnimInstance->Montage_SetEndDelegate(OnMontageEnded, InstanceData.MontageToPlay);
+	
+	if (InstanceData.bStopWhenExitState && AnimInstance->Montage_IsPlaying(InstanceData.MontageToPlay))
+	{
+		AnimInstance->Montage_Stop(0.2f, InstanceData.MontageToPlay);
+	}
+}
+
+#if WITH_EDITOR
+FName FST_PlayMontageAndWaitStateTreeTask::GetIconName() const
+{
+	return FName("");
+}
+
+FColor FST_PlayMontageAndWaitStateTreeTask::GetIconColor() const
+{
+	return FColor::Silver;
+}
+
+FText FST_PlayMontageAndWaitStateTreeTask::GetDescription(const FGuid& ID, FStateTreeDataView InstanceDataView,
+	const IStateTreeBindingLookup& BindingLookup, EStateTreeNodeFormatting Formatting) const
+{
+	const FInstanceDataType* InstanceData = InstanceDataView.GetPtr<FST_PlayMontageAndWaitTaskInstanceData>();
+	
+	if (InstanceData == nullptr)
+	{
+		return FText::GetEmpty();
+	}
+	
+	if (InstanceData->MontageToPlay == nullptr)
+	{
+		return NSLOCTEXT(
+			"FST_PlayMontageAndWaitStateTreeTask",
+			"Play Montage And Wait Task Description",
+			"Play Montage And Wait On Context Actor"
+		);
+	}
+	
+	return FText::Format(
+		NSLOCTEXT(
+		"FST_PlayMontageAndWaitStateTreeTask",
+		"Play Montage And Wait Task Description",
+		"Play {0} Montage And Wait On Context Actor"
+		),
+		FText::FromString(InstanceData->MontageToPlay->GetName())
+	);
+}
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Source/StateTreeCourse/Public/StateTree/Task/ST_PlayMontageAndWaitStateTreeTask.h
@@ -0,0 +1,70 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "StateTreeTaskBase.h"
+#include "ST_PlayMontageAndWaitStateTreeTask.generated.h"
+
+USTRUCT()
+struct FST_PlayMontageAndWaitTaskInstanceData
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(VisibleDefaultsOnly, Category=Context, meta=(DisplayName="Context Actor"))
+	TObjectPtr<APawn> ContextActor;
+	
+	UPROPERTY(EditAnywhere, Category=Parameter)
+	TObjectPtr<UAnimMontage> MontageToPlay;
+	
+	UPROPERTY(EditAnywhere, Category=Parameter)
+	float PlayRate = 1.0f;
+	
+	UPROPERTY(EditAnywhere, Category=Parameter)
+	FName StartSection = NAME_None;
+	
+	UPROPERTY(EditAnywhere, Category=Parameter)
+	bool bStopWhenExitState = true;
+	
+	bool bIsFinished = false;
+	bool bWasInterrupted = false;
+};
+
+USTRUCT(Category="AI|Animation", meta=(
+	DisplayName="Play Montage And Wait",
+	ToolTip="Play Montage And Wait On Context Actor",
+	Keywords="AI, Animation, Montage, StateTree"
+))
+struct FST_PlayMontageAndWaitStateTreeTask : public FStateTreeTaskCommonBase
+{
+	GENERATED_BODY()
+	
+	using FInstanceDataType = FST_PlayMontageAndWaitTaskInstanceData;
+	
+	virtual const UStruct* GetInstanceDataType() const override;
+	
+	virtual EStateTreeRunStatus EnterState(FStateTreeExecutionContext& Context, const FStateTreeTransitionResult& Transition) const override;
+	virtual EStateTreeRunStatus Tick(FStateTreeExecutionContext& Context, const float DeltaTime) const override;
+	virtual void ExitState(FStateTreeExecutionContext& Context, const FStateTreeTransitionResult& Transition) const override;
+	
+#if WITH_EDITOR
+	virtual FName GetIconName() const override;
+	virtual FColor GetIconColor() const override;
+	virtual FText GetDescription(const FGuid& ID, FStateTreeDataView InstanceDataView,
+		const IStateTreeBindingLookup& BindingLookup, EStateTreeNodeFormatting Formatting = EStateTreeNodeFormatting::Text) const override;
+#endif
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
